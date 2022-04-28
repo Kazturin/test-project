0f440f20633a5d461b26f96a23c7c980082e8e23
Arman <arman_096@bk.ru>
2022-04-28 14:48:31 +0600

    Init commit

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -6,6 +6,7 @@
         <path d="M20,19v1a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V19a6,6,0,0,1,6-6h4A6,6,0,0,1,20,19Z" fill="#464646"/>
       </svg>
       <span class="text-3xl">
+        {{user.name}}
       </span>
     </div>
     <hr>
